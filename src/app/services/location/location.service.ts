87c3880cf7a50efcd4534b7eb33b3a314ba36b44
pahs2554 <pahs2545@gmail.com>
2025-06-04 15:39:49 +0700

    edit commit

--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -6,19 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LocationService {
-  private baseUrl = 'https://backend-child-development-tracker-production.up.railway.app/api/location'; 
+  private baseUrl = 'https://backend-child-development-tracker-production.up.railway.app/api/location';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProvinces(): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/provinces`);
   }
 
-  getDistricts(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/districts`);
+  getDistricts(provinceId: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/districts/${provinceId}`);
   }
 
-  getSubDistricts(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/sub-districts`);
+  getSubDistricts(amphureId: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/sub-districts/${amphureId}`);
   }
 }
